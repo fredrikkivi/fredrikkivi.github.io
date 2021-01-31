3b2e0ff30fc0a901838643e085e1c74ed0811663
Fredrik Kivi <fredrik.kivi@ugli.se>
2021-01-31 23:46:26 +0100

    local-ci

---
+++ precache-manifest.b1be5ba3a6695ff1d8dd27384e915e47.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30425edee93c125b7b1c625be8f5949",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dfc8fbfafaa2530c5d6",
+    "url": "/static/css/main.c5a3ee5c.chunk.css"
+  },
+  {
+    "revision": "d201fb6d1f7e41b42c4c",
+    "url": "/static/js/2.abba0c9c.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.abba0c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dfc8fbfafaa2530c5d6",
+    "url": "/static/js/main.b399ec7c.chunk.js"
+  },
+  {
+    "revision": "deaf17d855cbdaf7344d",
+    "url": "/static/js/runtime-main.47788c5d.js"
+  },
+  {
+    "revision": "8dc8e18735667d4135721fe683aebf9e",
+    "url": "/static/media/bg0.8dc8e187.jpg"
+  },
+  {
+    "revision": "3fdd389de55b1591314c01975479a4c2",
+    "url": "/static/media/christian.3fdd389d.jpg"
+  },
+  {
+    "revision": "3fdd389de55b1591314c01975479a4c2",
+    "url": "/static/media/studio-1.3fdd389d.jpg"
+  },
+  {
+    "revision": "6d7dc059fb2944796b47a803b1118a6e",
+    "url": "/static/media/studio-2.6d7dc059.jpg"
+  },
+  {
+    "revision": "3af2b2fe1e2143011b5ceeb9a1af20d4",
+    "url": "/static/media/studio-4.3af2b2fe.jpg"
+  },
+  {
+    "revision": "eb1eb48e372aee79e45346ab7c1fc7f4",
+    "url": "/static/media/studio-5.eb1eb48e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e266ccaa090236204148abaa1c3668d.js"
+  "/precache-manifest.b1be5ba3a6695ff1d8dd27384e915e47.js"
 );
 
 self.addEventListener('message', (event) => {
